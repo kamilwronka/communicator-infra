07c91094fc2155a59ad2b8e2a46d5d458f4686b5
Kamil Wronka <contact@kamilwronka.com>
2022-11-07 21:41:39 +0100

    Added forwarded values as they are required

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -33,6 +33,14 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
 
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
